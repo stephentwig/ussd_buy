a2d395acb088109b886d8bbe812e6f32c70d2d6a
step.twig@gmail.com <step.twig@gmail.com>
2022-09-27 19:54:51 +0000

    add laravel breeze components

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::view('profile', 'profile')->name('profile');
+    Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])
+        ->name('profile.update');
+
+    Route::resource('tasks', \App\Http\Controllers\TaskController::class);
+});
+
+require __DIR__.'/auth.php';
