e2d6411c69f2079d76812814ba465c470ce2215e
stephentwig <step.twig@gmail.com>
2022-10-04 01:17:26 +0000

    update theses files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(TaskSeeder::class);
+//        $this->call(TaskSeeder::class);
 
         User::truncate();
         Contact::truncate();
